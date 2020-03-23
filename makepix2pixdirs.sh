180a1032226272332daff43e30a2cbefa1c9a39b
Wouter Heyndrickx <wheyndri@its.jnj.com>
2020-03-23 09:19:13 +0100

    cuda enabled pytorch and pix2pix dirs

---
+++ makepix2pixdirs.sh
@@ -0,0 +1,5 @@
+mkdir ExtractPitchLines/datasets/soccer_seg_detection
+mkdir ExtractPitchLines/datasets/soccer_seg_detection/test
+mkdir ExtractPitchLines/datasets/soccer_seg_detection/train_phase_1
+mkdir ExtractPitchLines/datasets/soccer_seg_detection/train_phase_2
+mkdir ExtractPitchLines/datasets/soccer_seg_detection/val
