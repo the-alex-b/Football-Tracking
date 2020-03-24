e52d99be0340a2cdce90c5a6c133341deeb5ecfa
Alex <alexbenou90@gmail.com>
2020-03-24 20:16:59 +0100

    Validated homography calculation and cleaned up code a bit

--- frame.py
+++ frame.py
@@ -18,6 +18,7 @@ class Frame:
         self.features = None
         self.temp_frame = None
         self.i = iteration
+        self.homography = None
 
         self.frame_height = frame.shape[0]
         self.frame_width = frame.shape[1]
@@ -37,8 +38,6 @@ class Frame:
         self.flann = pyflann.FLANN()
         self.retrieved_image = None
 
-
-      
         # Analyze the frame
         self.extract_pitch_lines()
         self.create_line_image()
@@ -96,20 +95,22 @@ class Frame:
 
         hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, n_bins)
 
-
         self.temp_frame = cv2.resize(self.pix_lines, (im_w,im_h))
 
         # compute hog and transpose so that shape matches the database features
         self.features = hog.compute(self.temp_frame).T
 
     def retrieve_a_camera(self):
-        print(self.database_features.shape)
-        print(self.features.shape)
+        # print(self.database_features.shape)
+        # print(self.features.shape)
 
         # Find the nearest neighbour feature match
         result, _ = self.flann.nn(self.database_features, self.features, 1, algorithm="kdtree", trees=16, checks=64)
         retrieved_index = result[0]
         # print("Retrieved index: "+str(retrieved_index))
+
+        # For faster testing on the 16.jpg image from SCCvSD repo
+        # retrieved_index = 46736
 
         # Find the camera that corresponds to the feature set that has been found to match
         retrieved_camera_data = self.database_cameras[retrieved_index]
@@ -120,35 +121,31 @@ class Frame:
         cc = retrieved_camera_data[6:9]
         retrieved_camera = ProjectiveCamera(fl, u, v, cc, rod_rot)
 
-        # Get the homography of the retrieved camera (still unrefined)
-        unrefined_homography = retrieved_camera.get_homography()
+        # Which homography to use? One corrected for the field or not (seems to mess with visualization, for calculation the first one might be better)
+        retrieved_homography = IouUtil.template_to_image_homography_uot(retrieved_camera, self.template_h, self.template_w)
+        # retrieved_homography = retrieved_camera.get_homography()
 
         # Turn the camera to an image with a template
-        self.retrieved_image = SyntheticUtil.camera_to_edge_image(retrieved_camera_data, self.model_points, self.model_line_index,
-                                                            im_h=self.frame_height, im_w=self.frame_width, line_width=4)
+        self.retrieved_image = SyntheticUtil.camera_to_edge_image(retrieved_camera_data, self.model_points, self.model_line_index, im_h=self.frame_height, im_w=self.frame_width, line_width=4)
         self.pix_lines = cv2.resize(self.pix_lines, (1280, 720), interpolation=cv2.INTER_CUBIC)[:, :, None]
         
         # Refine using lucas kanade algorithm
         dist_threshold = 50
-
         query_dist = SyntheticUtil.distance_transform(self.pix_lines)
         retrieved_dist = SyntheticUtil.distance_transform(self.retrieved_image)
-
         query_dist[query_dist > dist_threshold] = dist_threshold
         retrieved_dist[retrieved_dist > dist_threshold] = dist_threshold
 
         warp = SyntheticUtil.find_transform(retrieved_dist, query_dist)
-
-     
-
-        # Is this correct? Need to check! Should it be dot?
-        self.homography = np.matmul(unrefined_homography, warp)
         
-        # Use the warp found in the refinement step to transorm the retrieved camera data and draw field lines. For nicer visualisation we should warp the camera features and redraw the field (so lines are not cut off)
+        # Use the homography to refine the found image to validate correctness.
         self.refined_retrieved_image = cv2.warpPerspective(self.retrieved_image, warp, (1280, 720))
 
+        # This is the homography mapping the field to a top down view
+        self.homography = warp@retrieved_homography
 
-
+        # For validation: warp an image to top view:
+        # self.normalized_image = cv2.warpPerspective(self.original, np.linalg.inv(self.homography),(1280,720))
 
     def visualize(self):
         cv2.imshow('adapted', self.pix_lines)
@@ -159,9 +156,11 @@ class Frame:
         cv2.waitKey(40000)
 
     def save(self):
-        print("start saving")
         cv2.imwrite('./output_images/{}_original.jpg'.format(self.i), self.original)
         cv2.imwrite('./output_images/{}_pix2pix.jpg'.format(self.i), self.pix_lines)
         # cv2.imwrite('./output_images/{}_retrieved.jpg'.format(self.i), self.retrieved_image)
         cv2.imwrite('./output_images/{}_refined.jpg'.format(self.i), self.refined_retrieved_image)
+        # cv2.imwrite('./output_images/{}_normalized.jpg'.format(self.i), self.normalized_image)
+        # cv2.imwrite('./output_images/{}_unwarped_image.jpg'.format(self.i), self.unwarped_image)
+        
 
