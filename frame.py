b6fd28488f86c9df01bdfc40989e7273beeb27a0
Wouter Heyndrickx <wheyndri@its.jnj.com>
2020-04-19 18:25:53 +0200

    reducing number of object attributes and further spliting into functions

--- frame.py
+++ frame.py
@@ -20,7 +20,6 @@ class Frame:
         self.original = frame
         self.features = None
         self.i = identifier
-        self.homography = None
         self.resolution = (frame.shape[1],frame.shape[0])
         self.database_features = database_features
         self.database_cameras = database_cameras
@@ -33,6 +32,7 @@ class Frame:
         self.template_w = 115
 
         self.nnsearcher = nnsearcher
+        self.picstosave = []
         
 
     def process(self): 
@@ -45,6 +45,7 @@ class Frame:
         # Visualize player coordinates as circles below players:
         for c in self.playercoos:
             cv2.circle(self.original,(int(c[0]), int(c[1])), 5, (0,0,255), 3)
+        self.picstosave.append(('original',self.original))
         
 
         # Call all functions that extract data from the frame to determine homography
@@ -52,25 +53,26 @@ class Frame:
         pix_lines = self.plot_lines_on_image(pix_lines)
         features = self.generate_hog_features(pix_lines)
         pix_lines, retrieved_image, retrieved_homography = self.retrieve_a_camera(pix_lines, features)
-        self.refine_camera(pix_lines, retrieved_image, retrieved_homography)
-        self.calculate_2d_coordinates()
-        twodvis.twodvisualisation(self.warped_coords, self.i, self.template_w, self.template_h)
+        final_homography = self.refine_camera(pix_lines, retrieved_image, retrieved_homography)
+        twod_coords = self.calculate_2d_coordinates(final_homography)
+        self.create_normalizedview(self.original, final_homography)
+        self.create_overlayedview()
+        twodvis.twodvisualisation(twod_coords, self.i, self.template_w, self.template_h)
         # Warped coords belonging to worldcup image 16 (placeholder for faster development)
         # self.warped_coords = [[92.01287027834677, 28.786941200521106], [94.66302995069726, 67.04521116566309], [95.50522352407108, 60.511763034888595], [97.82874971791972, 42.39573348700747], [91.24491420783876, 56.98150085508616], [113.06896849777085, 38.37172302960884], [90.4490186162751, 62.02393262415963], [80.46709610719846, 44.462272402932655], [88.0790275606092, 19.51399275883541], [97.8791538613156, 35.674698750579715], [81.00846673234102, 38.831727152810615], [78.82873504463414, 26.249622960034305], [87.45029855143562, 36.598743959497696], [99.58510108821706, 33.51209547287334], [78.72732178814238, 20.72997452258794]
         # Visualize or save the frame:
         # self.visualize()
-        self.save(pix_lines)
+        self.save(self.picstosave)
 
 
-    def calculate_2d_coordinates(self): 
+    def calculate_2d_coordinates(self, homography): 
         # Use inverse of the homography to calculate topview coordinates of players
-        self.warped_coords = [] 
+        warped_coords = [] 
         for c in self.playercoos:
             c_mat = np.array([[c[0]],[c[1]],[1]])
-
-            hom_cords = np.linalg.inv(self.homography)@c_mat
-
-            self.warped_coords.append([hom_cords[0][0]/hom_cords[2][0],hom_cords[1][0]/hom_cords[2][0]])
+            hom_cords = np.linalg.inv(homography)@c_mat
+            warped_coords.append([hom_cords[0][0]/hom_cords[2][0],hom_cords[1][0]/hom_cords[2][0]])
+        return warped_coords
 
 
     # Pix2Pix model to extract lines and field area 
@@ -110,6 +112,8 @@ class Frame:
     def plot_lines_on_image(self, pix_lines):
         pix_lines = cv2.cvtColor(pix_lines, cv2.COLOR_BGR2GRAY)
         pix_lines = cv2.threshold(pix_lines, 127, 255, cv2.THRESH_BINARY)[1]
+        #self.save('pix2pix',pix_lines)
+        self.picstosave.append(('pix2pix',pix_lines))
         return pix_lines
 
     def generate_hog_features(self, pix_lines):
@@ -148,6 +152,9 @@ class Frame:
 
         # Turn the camera to an image with a template
         retrieved_image = SyntheticUtil.camera_to_edge_image(retrieved_camera_data, self.model_points, self.model_line_index, im_h=self.resolution[1], im_w=self.resolution[0], line_width=4)
+        #self.save('retrieved',retrieved_image)
+        self.picstosave.append(('retrieved',retrieved_image))
+        
         pix_lines = cv2.resize(pix_lines, self.resolution, interpolation=cv2.INTER_CUBIC)[:, :, None]
 
         return pix_lines, retrieved_image, retrieved_homography
@@ -161,28 +168,30 @@ class Frame:
         retrieved_dist[retrieved_dist > dist_threshold] = dist_threshold
 
         warp = SyntheticUtil.find_transform(retrieved_dist, query_dist)
-        
-        # Use the homography to refine the found image to validate correctness.
+        # This is the homography mapping the field to a top down view
+        homography = warp@retrieved_homography
+
         self.refined_retrieved_image = cv2.warpPerspective(retrieved_image, warp, self.resolution)
+        self.picstosave.append(('refined',self.refined_retrieved_image))
+        return homography
+        
+    def create_overlayedview(self): 
         self.original = cv2.resize(self.original,self.resolution, interpolation=cv2.INTER_CUBIC)
         self.overlayed_image = cv2.addWeighted(self.original,0.4,self.refined_retrieved_image,0.1,0)
+        self.picstosave.append(('overlayed_image',self.overlayed_image))
 
-        # This is the homography mapping the field to a top down view
-        self.homography = warp@retrieved_homography
-
+    def create_normalizedview(self, original, homography): 
+        # Use the homography to refine the found image to validate correctness.
         # For validation: warp an image to top view:
-        self.normalized_image = cv2.warpPerspective(self.original, np.linalg.inv(self.homography),self.resolution)
+        normalized_image = cv2.warpPerspective(original, np.linalg.inv(homography),self.resolution)
+        self.picstosave.append(('normalized',normalized_image))
+        return 
 
     def visualize(self, label, pic):
         cv2.imshow(label, pic)
 
-    def save(self, pix_lines):
-        cv2.imwrite('./output_images/{}_original.jpg'.format(self.i), self.original)
-        cv2.imwrite('./output_images/{}_pix2pix.jpg'.format(self.i), pix_lines)
-        #cv2.imwrite('./output_images/{}_retrieved.jpg'.format(self.i), self.retrieved_image)
-        #cv2.imwrite('./output_images/{}_refined.jpg'.format(self.i), self.refined_retrieved_image)
-        #cv2.imwrite('./output_images/{}_normalized.jpg'.format(self.i), self.normalized_image)
-        #cv2.imwrite('./output_images/{}_unwarped_image.jpg'.format(self.i), self.unwarped_image)
-        #cv2.imwrite('./output_images/{}_overlayed_image.jpg'.format(self.i), self.overlayed_image)
+    def save(self, tosave):
+        for e in tosave: 
+            cv2.imwrite('./output_images/{}_{}.jpg'.format(self.i, e[0]), e[1])
         
 
